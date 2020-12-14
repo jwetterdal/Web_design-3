3a9481522a7bd08de599e305be6b39a6f3d903b0
jwetterdal <56357769+jwetterdal@users.noreply.github.com>
2020-12-14 10:18:36 -0800

    pages

--- Final/script.js
+++ Final/script.js
@@ -31,13 +31,53 @@ $(document).ready(function () {
         interval: 3000,
     });
 
-    // $('#homePageCarousel').on('slid.bs.carousel', function () {
-    //     console.log(this);
-    //     $('.logo').each(function(){
+ //....Chart
 
-    //         new CircleType(this).dir(-1).radius(384);
-    //     })
-    // })
+    $("form.needs-validation").submit(function (event) {
+        event.preventDefault();
+
+        if ($("input#exampleInputEmail1").val() === "") {
+            $("form.needs-validation").removeClass('needs-validation').addClass('was-validated');
+        }
+    });
+
+    $("#exampleModal").modal('show');
+
+    $("#inlineCheckbox1").click(function () {
+        copyInfo(document.getElementById('myForm'));
+    });
+
+    (function () {
+        'use strict';
+        window.addEventListener('load', function () {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
+
+    var hide = function() {
+  var cb = document.getElementById("checkbox");
+  var shipping = document.getElementsByClassName("shipping");
+  if (cb.checked) {
+    for (var i=0; i<shipping.length; i++) {
+      shipping[i].classList.add("hide");
+    }
+  } else {
+    for (var i=0; i<shipping.length; i++) {
+      shipping[i].classList.remove("hide");
+    }
+  }
+}
 
 
 });
